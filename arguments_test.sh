814644764ba4fe1a5dc100ffe8588c2dddaa2dc8
karakyrt <44345658+karakyrt@users.noreply.github.com>
2020-05-10 17:53:33 -0500

    Update arguments_test.sh

--- arguments_test.sh
+++ arguments_test.sh
@@ -20,10 +20,10 @@ echo "======================="
 # $?------iakuiy prenadlezhutiy=0(iaksho programa spraciiyvala to vona pokazala na vuxodi 0 seccess)
 # $*------bere argymentu i pakazyie ii v odniiy liniii
 
-# stdout------vudaie output na ekran abo v faiylu i papku(stdout=2) 
-# stdin-------bere i vvodutiy shosiy input something(stdout=0)
-# stderror----vudaie output na ekran iaki ie errors(stderrors=1)
-# exemple:
+# stdout------vudaie output na ekran abo v faiylu i papku(stdout=1) abo rezylitat komandu ls(file1, file2, file3, file4)
+# stdin-------bere i vvodutiy shosiy input something(stdout=0) to sho tu vviv v terminal napruklad kamandy: ls
+# stderror----vudaie output na ekran iaki ie errors(stderrors=2) abo vudaie errors iaki vunuklu v procesi robotu ciieii(ls) abo inshoii
+# exemple:                                                                                                       komandu
 # echo "andrey" | wc
 # wc byde praciyvatu na outputom komandu echo "andrey" toiistiy sho pokazhe stdout echo "andrey" i nad cum outputom byde praciiyvatu wc
 
